55156c198b0ffff036f92c199bbb31480563f2ee
PedroFCT <rm93611@fiap.com.br>
2023-10-26 10:11:27 -0300

    ultima missao

--- src/main/java/br/com/pdrprojects/onebasketball/config/SecurityConfig.java
+++ src/main/java/br/com/pdrprojects/onebasketball/config/SecurityConfig.java
@@ -8,12 +8,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    UserRepository repository;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
             .authorizeHttpRequests( auth -> auth.anyRequest().authenticated() )
             .oauth2Login(form -> form.loginPage("/login").defaultSuccessUrl("/partida").permitAll() )
             .logout ( logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login"))
+            .addFilterBefore(new LoginFilter(repository), OAuth2LoginAuthenticationFilter.class)
             .build();
     }
     
