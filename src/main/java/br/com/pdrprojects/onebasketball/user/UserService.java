b73322e147f6d52da7194144ab7c7def0f2a9c87
PedroFCT <rm93611@fiap.com.br>
2023-10-31 02:20:57 -0300

    missao final?

--- src/main/java/br/com/pdrprojects/onebasketball/user/UserService.java
+++ src/main/java/br/com/pdrprojects/onebasketball/user/UserService.java
@@ -12,18 +12,18 @@ public class UserService {
     UserRepository repository;
 
     public void addScore(User githubuser, Integer score) {
-        //buscar usr do bd
+        
         Optional<User> opt = repository.findById(githubuser.getId());
 
-        //verificar se existe
+        
         if (opt.isEmpty())
             throw new RuntimeException("usuário não encontrado");
 
-        //somar os pontos
+        
         var user = opt.get();
         user.setScore(user.getScore() + score);
 
-        //salvar
+       
         repository.save(user);
     }
 
